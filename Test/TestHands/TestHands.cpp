e843e896f26ec0aa677ac43fe334c0bb337c0042
Suui <suui.gd@gmail.com>
2015-06-11 19:00:16 +0200

    Test written

--- Test/TestHands/TestHands.cpp
+++ Test/TestHands/TestHands.cpp
@@ -4,6 +4,7 @@ TEST_CASE("Testing Hand Input")
 {
 	SECTION("High Card")
 	{
-		
+		Result Result = HandChecker::check("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH");
+		CHECK(Result.GetOutput() == "White wins. - with high card: Ace");
 	}
 }
