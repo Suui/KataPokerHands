a924fe29db53629ccb5b2b5d4612b172e1fe0799
Suui <suui.gd@gmail.com>
2015-06-11 20:30:12 +0200

    Green: Hand Color is White

--- Test/TestHands/TestCards.cpp
+++ Test/TestHands/TestCards.cpp
@@ -4,9 +4,15 @@
 
 TEST_CASE("Testing Cards")
 {
-	SECTION("Hand Color")
+	SECTION("Hand Color is Black")
 	{
 		Hand Hand = HandBuilder::build("Black: 2H 3D 5S 9C KD");
 		CHECK(Hand.GetColor() == Color::BLACK);
+	}
+
+	SECTION("Hand Color is White")
+	{
+		Hand Hand = HandBuilder::build("White: 2C 3H 4S 8C AH");
+		CHECK(Hand.GetColor() == Color::WHITE);
 	}
 }
