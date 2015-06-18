34812d135600bac0f4baddef6e4824a9274abb43
Suui <suui.gd@gmail.com>
2015-06-18 20:52:49 +0200

    Green: An input of One Pair and a High Card

--- Test/Testing/TestingGames.cpp
+++ Test/Testing/TestingGames.cpp
@@ -4,11 +4,17 @@
 
 SCENARIO("Testing Games")
 {
-	GIVEN("An input of two Highest Cards")
+	GIVEN("An input of two High Cards")
 	{
 		Result result = Game().Play("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH");
 		CHECK(result.Winner().Color() == WHITE);
 		CHECK(result.Winner().Play() == HIGH_CARD);
 	}
 	
+	GIVEN("An input of One Pair and a High Card")
+	{
+		Result result = Game().Play("Black: 2H 3D 5S 9C 2D  White: 2C 3H 4S 8C AH");
+		CHECK(result.Winner().Color() == BLACK);
+		CHECK(result.Winner().Play() == ONE_PAIR);
+	}
 }
